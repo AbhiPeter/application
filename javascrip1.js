90a7abd16ac425e168b46a36bd43634602bc22a5
AbhiPeter <more.abhijit667@gmail.com>
2019-01-08 16:56:53 +0530

    code for application

---
+++ javascrip1.js
@@ -0,0 +1,50 @@
+//document.addEventListener("DOMContentLoaded", function(event) {
+
+var ulElement = document.querySelector(".list-group");
+
+var liList = document.querySelectorAll(".list-group-items");
+var spanStudent = document.querySelectorAll(".technology");
+var spanDelete = document.querySelectorAll(".remove")
+console.log(liList);
+console.log(spanStudent);
+
+ulElement.addEventListener("click", function(event) {
+    if (event.target.className == "remove") {
+        event.target.parentElement.remove();
+    }
+}, false)
+
+liList.forEach(function(element, index, nodeList) {
+    console.log("hello enter in this code");
+    var spanDelete = element.querySelector(".remove");
+    spanDelete.addEventListener("click", function(event) {
+        element.remove();
+        spanDelete.parentElement.remove();
+    })
+})
+
+//adding the list in your search box is given here
+function addItem() {
+    event.preventDefault();
+
+    var inputValue = document.querySelector('.form-group input');
+    var li = document.createElement("li");
+    var spanStudent = document.createElement("span");
+    var spanDelete = document.createElement("span");
+
+    li.className = "list-group-items";
+    spanStudent.className = "technology";
+    spanDelete.className = "remove";
+
+    li.append(spanStudent);
+    li.append(spanDelete);
+
+    ulElement.append(li);
+
+    spanStudent.innerText = inputValue.value;
+    spanDelete.innerText = "Remove";
+    inputValue.value = "";
+}
+
+
+//})
